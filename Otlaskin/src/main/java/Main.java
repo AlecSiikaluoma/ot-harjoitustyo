842ffbeab8cc55cc1cff844b30dfd685fc961511
Alec Siikaluoma <alec.siikaluoma@helsinki.fi>
2018-11-19 19:40:07 +0200

    Add simple user interface

--- Otlaskin/src/main/java/Main.java
+++ Otlaskin/src/main/java/Main.java
@@ -1,31 +1,22 @@
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        //creating a Group object
-        Group group = new Group();
-
-        //Creating a Scene by passing the group object, height and width
-        Scene scene = new Scene(group ,600, 300);
-
-        //setting color to the scene
-        scene.setFill(Color.BROWN);
-
-        //Setting the title to Stage.
-        primaryStage.setTitle("Sample Application");
-
-        //Adding the scene to Stage
-        primaryStage.setScene(scene);
-
-        //Displaying the contents of the stage
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Otlaskin.fxml"));
+        primaryStage.setTitle("Otlaskin");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, 380, 460));
         primaryStage.show();
     }
-    public static void main(String args[]){
+
+
+    public static void main(String[] args) {
         launch(args);
     }
 }
--- dokumentaatio/tyoaikakirjanpito.md
+++ dokumentaatio/tyoaikakirjanpito.md
@@ -4,3 +4,4 @@
 | :----:|:-----| :-----|
 | 11.11. | 1    | Määrittelydokumentti ja aiheen valitseminen |
 | 12.11 | 2 | Projektin suunnittelu ja JavaFX:n opettelu |
+| 19.11 | 3 | fxml:n lisäys ja alustavan käyttöliittymän teko |
